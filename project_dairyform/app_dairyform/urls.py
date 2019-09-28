6a21ed5128a06f2cbdb40cf2247e1688f27c8f0d
yeswanth <yeswanthcse2013@gmail.com>
2019-09-28 14:52:14 +0530

    modified project urls

--- project_dairyform/app_dairyform/urls.py
+++ project_dairyform/app_dairyform/urls.py
@@ -3,11 +3,10 @@ from .import views
 from app_dairyform import views
 #from app_dairyform import views
 urlpatterns = [
-    path('', views.FarmerFormView.as_view(), name='FarmerFormView'),
     path('app_dairyform/', views.FarmerFormView.as_view(), name='FarmerFormView'),
     path('',views.home,name='home'),
     path('123/',views.about),
     path('543/',views.email),
-    # path('',views.response),
+    path('',views.response),
     # path('', views.FarmerFormView.as_view(), name='FarmerFormView'),
 ]
--- project_dairyform/project_dairyform/urls.py
+++ project_dairyform/project_dairyform/urls.py
@@ -19,9 +19,9 @@ from app_dairyform import views
 from django.urls import path, include
 
 urlpatterns = [
+    path('',include('app_dairyform.urls')),
     path('app_dairyform/', include('app_dairyform.urls')),
     path('admin/', admin.site.urls),
-    path('',include('app_dairyform.urls')),
     path('about/',include('app_dairyform.urls')),
     path('email us/',include('app_dairyform.urls')),
     path('app_dairyform/', include('app_dairyform.urls')),
